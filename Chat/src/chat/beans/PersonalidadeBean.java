b34f5c9cc45a4bc432ec4f7b355c98fb824cb907
Almir Fontenele <almir.oliveira@supportcomm.com.br>
2016-12-05 15:17:39 -0200

    Pra fazer o dialout

--- Chat/src/chat/beans/PersonalidadeBean.java
+++ Chat/src/chat/beans/PersonalidadeBean.java
@@ -1,8 +1,13 @@
 package chat.beans;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
+import br.com.supportcomm.dialout.util.Dialout;
 import chat.entities.Personalidade;
 
 @ManagedBean(name = "personalidadeBean")
@@ -20,8 +25,63 @@ public class PersonalidadeBean {
 
 	public String entrar(){
 		
+		try {
+			List<Dialout> dialouts = new ArrayList<Dialout>();
+			Dialout dialout = new Dialout();
+			List<String> variable = new ArrayList<String>();
+			
+			
+			variable.add( "CALLERID(DNID)=8486" );
+    		variable.add("destinationNumber=".concat(this.personalidade.getMsisdnPersonalidade()));
+			
+			dialout.setChannel("SIP/" + this.personalidade.getMsisdnPersonalidade());
+			dialout.setExten("8486");
+			dialout.setIpConnection("192.168.3.108");
+			dialout.setLoginAsterisk("manager");
+			dialout.setPassAsterisk("senha");
+			dialout.setMsisdnOriginator("8486");
+			dialout.setPriority(1);
+			dialout.setVariables(variable );
+			dialout.setContext("default");
+			dialout.run();
+			
+			
+			Thread threadDoPdf = new Thread(dialout);
+        	threadDoPdf.start();
+        	dialouts.add(dialout);
+			
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
 		return null;
 		
 	}
 
+	
+	 private class Channel{
+		 private String msisdn;
+		 private String msisdnOriginator;
+		public String getMsisdn() {
+			return msisdn;
+		}
+		public void setMsisdn(String msisdn) {
+			this.msisdn = msisdn;
+		}
+		public String getMsisdnOriginator() {
+			return msisdnOriginator;
+		}
+		public void setMsisdnOriginator(String msisdnOriginator) {
+			this.msisdnOriginator = msisdnOriginator;
+		}
+			
+	 }
+	
+	
+	
 }
